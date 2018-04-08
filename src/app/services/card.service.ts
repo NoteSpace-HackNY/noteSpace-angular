b7a6fbe3e8146022d01bed7f6c42411cee6b1df3
Taranvir Waraich <taranvir@me.com>
2018-04-08 06:58:25 -0400

    Created the card service methods

--- src/app/services/card.service.ts
+++ src/app/services/card.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
+import { Card } from '../models/card.model';
+import { Observable } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class CardService {
 
-  constructor() { }
+  constructor(
+    private apiService: ApiService,
+  ) { }
+
+  getAll(workspace_id: number): Observable<Card[]> {
+    return this.apiService.get<Card[]>('api/card/', [{
+      key: 'workspace_id', value: workspace_id.toString()
+    }]);
+  }
+
+  get(card_id: number): Observable<Card> {
+    return this.apiService.get<Card>(`api/card/${card_id}/`)
+  }
+
+  create(card: Card): Observable<Card> {
+    return this.apiService.post<Card>('api/card/', card);
+  }
+
+  update(card: Card): Observable<Card> {
+    return this.apiService.put<Card>('api/card/', card);
+  }
+
+  delete(card_id: number): Observable<any> {
+    return this.apiService.delete(`api/card/${card_id}/`);
+  }
 
 }
